bf128a0b70db0ab88d855bb289b21dd4a4aed358
Robbie Rosati <robbie@robbierosati.space>
2026-06-01 09:56:27 -0500

    more portable build

--- all_wave_runs_coarse.sh
+++ all_wave_runs_coarse.sh
@@ -6,7 +6,7 @@ NSTEPS=1000
 DURATION=$((7680*338*24))
 NCH=6
 
-RUNDIR=wdmruns-coarse2/stat-noise-conf-ln-detection
+RUNDIR=wdmruns-coarse3/stat-noise-conf-ln-detection
 mkdir -p $RUNDIR
 nice -n 15 build/apps/src/noise_wavelet_mcmc \
         --steps $((10*$NSTEPS)) \
@@ -24,7 +24,26 @@ nice -n 15 build/apps/src/noise_wavelet_mcmc \
         --sim-noise
 #        --stationary-conf \
 
-RUNDIR=wdmruns-coarse2/noise-conf-ln-169-detection
+RUNDIR=wdmruns-coarse3/statws-noise-conf-ln-detection
+mkdir -p $RUNDIR
+nice -n 15 build/apps/src/noise_wavelet_mcmc \
+        --steps $((10*$NSTEPS)) \
+        --chains $NCH \
+        --threads $NCH \
+        --cheat \
+        --fmin 5e-4 \
+        --fmax 8e-3 \
+        --duration $DURATION \
+        --stationary \
+        --conf-noise \
+        --sgwb-template 1 \
+        --coarse-Q 1 \
+        --ws-approx \
+        --rundir $RUNDIR \
+        --sim-noise
+#        --stationary-conf \
+
+RUNDIR=wdmruns-coarse3/noise-conf-ln-169-detection
 mkdir -p $RUNDIR
 nice -n 15 build/apps/src/noise_wavelet_mcmc \
         --steps $((10*$NSTEPS)) \
@@ -40,7 +59,24 @@ nice -n 15 build/apps/src/noise_wavelet_mcmc \
         --rundir $RUNDIR \
         --sim-noise
 
-RUNDIR=wdmruns-coarse2/noise-conf-ln-1-detection
+RUNDIR=wdmruns-coarse3/noise-conf-ln-ws169-detection
+mkdir -p $RUNDIR
+nice -n 15 build/apps/src/noise_wavelet_mcmc \
+        --steps $((10*$NSTEPS)) \
+        --chains $NCH \
+        --threads $NCH \
+        --cheat \
+        --fmin 5e-4 \
+        --fmax 8e-3 \
+        --duration $DURATION \
+        --conf-noise \
+        --sgwb-template 1 \
+        --coarse-Q 169 \
+        --ws-approx \
+        --rundir $RUNDIR \
+        --sim-noise
+
+RUNDIR=wdmruns-coarse3/noise-conf-ln-1-detection
 mkdir -p $RUNDIR
 nice -n 15 build/apps/src/noise_wavelet_mcmc \
         --steps $NSTEPS \
